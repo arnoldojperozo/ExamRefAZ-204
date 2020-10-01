94153300a55169c6c278185e904d2eb07915137f
Arnoldo Perozo <arnoldo.perozo@hotmail.com>
2020-10-01 17:22:30 -0600

    Sample App

---
+++ Chapter01-IaaS-ComputeSolutions/WebApp/Controllers/TodoController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "a new todo item" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems() => await _context.TodoItems.ToListAsync();
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+
+    }
+}
---
+++ Chapter01-IaaS-ComputeSolutions/WebApp/README.md
@@ -0,0 +1,2 @@
+# todoAPI
+.NET core API app
